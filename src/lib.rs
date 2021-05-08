ee6dba8f85990d8ff17359d043ee3ce73baeeb85
Tom Rawcliffe <tom.rawcliffe@gmail.com>
2021-05-09 00:43:44 +0100

    Drawing a square

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,25 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 use web_sys::WebGlRenderingContext as GL;
 
+#[macro_use]
+extern crate lazy_static;
+
+mod app_state;
+mod common_funcs;
 mod gl_setup;
+mod shaders;
+mod programs;
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
+    pub fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub struct GameClient {
     gl: GL,
+    program_color_2d: programs::Color2D,
 }
 
 #[wasm_bindgen]
@@ -22,15 +30,29 @@ impl GameClient {
         console_error_panic_hook::set_once();
         let gl = gl_setup::initialize_webgl_context().unwrap();
         Self {
+            program_color_2d: programs::Color2D::new(&gl),
             gl: gl,
         }
     }
 
-    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+    pub fn update(&mut self, time: f32, height: f32, width: f32) -> Result<(), JsValue> {
+        app_state::update_dynamic_data(time, height, width);
         Ok(())
     }
 
     pub fn render(&self) {
         self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+
+        let curr_state = app_state::get_curr_state();
+
+        self.program_color_2d.render(
+            &self.gl,
+            curr_state.control_bottom,  //bottom
+            curr_state.control_top, //top
+            curr_state.control_left,  //left
+            curr_state.control_right, //right
+            curr_state.canvas_height, //canvas_height
+            curr_state.canvas_width, //canvas_width
+        );
     }
 }
